9c05062b9a326f3e3ba79ce89e2d38a02d61923a
Nikkolai Davenport <nikkolai@unity3d.com>
2015-07-23 22:04:24 -0700

    Improve ButtonExample
    - Remove class dependencies
    - Reset cooldown if (1) run in editor or development build is enabled,
    and (2) the application has been restarted since the last reward was
    earned

--- Assets/UnityAdsHelper/Examples/Scripts/UnityAdsExample.cs
+++ Assets/UnityAdsHelper/Examples/Scripts/UnityAdsExample.cs
@@ -4,24 +4,8 @@ using System;
 
 public class UnityAdsExample : MonoBehaviour 
 {
-	public static Action onResetCooldownEvent;
-
 	void Start ()
 	{
-		if (Debug.isDebugBuild)
-		{
-			OnResetCooldown();
-		}
-
 		UnityAdsHelper.Initialize();
-	}
-
-	public void OnResetCooldown ()
-	{
-		if (onResetCooldownEvent != null) 
-		{
-			Debug.Log("Resetting cooldowns...");
-			onResetCooldownEvent();
-		}
 	}
 }
